72e46baf14781a6dd7ca80c4af8d584addc7212b
Shardar Quraishi <ShardarQuraishi@users.noreply.github.com>
2019-06-22 21:18:18 +0600

    as2

---
+++ Assignment 2/as2.java
@@ -0,0 +1,38 @@
+public class BankAccount{
+	double balance;
+	double overdraftAmount = 100.0;    //instance variables
+
+	public void deposit(double money){  //deposit method to deposit money into account
+
+		if (money < 0){           //if statement to not be able to deposit negative amount
+			balance = balance;
+		}
+		else{
+			balance = balance + money;
+		}
+	}
+
+	public void withdraw(double money){      //withdraw method to withdraw money from account
+		if (balance <= 0){
+			if (money >= overdraftAmount){    // if statement to not be able to withdraw more than overdraft amount
+
+				balance = 0;
+			}
+		}
+		else{
+			balance = balance - money;
+		}
+
+	}
+
+	public double getBalance (){  //getter method
+		return balance;
+		}
+
+	public void setOverdraftAmount(double overdraftAmount){   //setter method
+		this.overdraftAmount = overdraftAmount;
+	}
+
+
+
+}
